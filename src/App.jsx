09f80a2f595001b0e1a11e377db7b403a0558c9f
IVAN <danilchenco36@gmai.com>
2024-02-01 13:44:49 +0600

    Добавлены комментарии

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react' /*Имортируем useState в React. И*/
+import './App.css' /*Импортируем стили */
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick /*Передача свойства Value и OnSquareClick, является props.*/}) {
   return (
-    <button className="square" onClick={onSquareClick}>
-    {value}
-    </button>
+    <button className="square"/*Реквизит или свойство, которое сообщается CSS для оформления кнопки*/ onClick={onSquareClick /*При нажатие на квадрат будет вызываться функция*/} > 
+    {value /*Выводим переменную Value, которая хранит значения(прописываем фигурные скобки)Также эти значения внтури JSX, являются  props*/} 
+    </button> /*Является JSX элементом. Комбинация кода JS и HTML-тегов*/ 
   );
 }
 
-function Board({xIsNext, squares, onPlay}){
-  function handleClick(i){
-    if (squares[i] || calculateWinner(i)){
-      return;
+function Board({xIsNext, squares, onPlay}) /*Функционал Board*/{
+  function handleClick(i)/*компонент функции handleClick*/ {
+    if (calculateWinner(squares) || squares[i])/*Проверка, выиграет игрок или нет.*/{
+      return/*Что следует после, возвращается, как значения функций. Если квадрат заполнен, то return запустит функцию handleClick раньше, чем доска успеет обновиться*/;
     }
-    const nextSquares = squares.slice();
-    
+    const nextSquares = squares.slice()/*Создает копию массива nextSquares с помощю метода array в JS slice. Slice - метод позволяет вернуть новый массив, который содержит копии элементов из исходного массива*/;
     if (xIsNext) {
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
+    /*Если массив nextSquares имеет ход X, то следующий ход будет O. i - аргумент, который принимает индекс квадрата(будет обновляться любой квадрат).*/
     onPlay(nextSquares);
   }
   
-  const winner = calculateWinner(squares);
-  let status;
+  const winner = calculateWinner(squares); /*переменная, значения которой не изменяются*/;
+  let status/*переменная, которая будет выводить значения внутри блока*/;
   if (winner) {
     status = "Winner: " + winner;
   } else {
-    status = "Next player: " + (xIsNext? "X" : "O");
+    status = "Next player: " + (xIsNext ? 'X' : 'O');
   }
-
-  return (
-    <>
-    <div className="status">{status}</div>
-    <div className="board-row">
+/*Если победитель выиграет в блоке выведиться "Winner: X или O", а если ход продолжается, то будет "Next player: X или O"*/
+  return /*Что следует после, возвращается, как значения функций*/(
+    /*используем данные скобки, чтобы получить несколько смежных фрагментов*/ <> 
+    <div className="status">{status/*выводим status*/}</div>
+    <div className="board-row"/*Свойство CSS*/>
      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
     </div>
-    <div className="board-row">
+    <div className="board-row"/*Свойство CSS*/>
       <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
       <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
       <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
     </div>
-    <div className="board-row">
+    <div className="board-row"/*Свойство CSS*/>
       <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
       <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
       <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
     </div>
-    </>
+    </> /*используем данные скобки, чтобы получить несколько смежных фрагментов*/
   );
   }
 
-export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([Array(9).fill(null)]);
+export default function Game() /*Сообщает о том, что данная функция является основной в этом коде, и становиться доступной за пределами файлов*/ {
+  const [xIsNext, setXIsNext] = useState(true); /*объявляет, что X будет ходить первым*/
+  const [history, setHistory] = useState([Array(9).fill(null)]) /*Создается массив из 9 элементов, который присвает значение null.useState объявляет вокруг squares переменную состояние,для которого изначально задано значение из массива */;
   const currentSquares = history[history.length - 1];
   function handlePlay(nextSquares){
-  setHistory([...history, nextSquares]);
-  setXIsNext(!xIsNext);
+    setHistory([...history, nextSquares]); /*Cоздается новый массив, который содержит все элементы history, то есть он будет их перечислять*/
+    setXIsNext(!xIsNext);
   }
-  return (
+  return ( /*Что следует после, возвращается, как значения к функциям*/
   <div className="game">
     <div className="game-board">
       <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
@@ -75,6 +73,11 @@ export default function Game() {
   </div>
  )
   }
+
+function jumpTo(nextMove) {
+//todo
+}
+
  function calculateWinner (squares) {
   const lines = [
     [0, 1, 2],
